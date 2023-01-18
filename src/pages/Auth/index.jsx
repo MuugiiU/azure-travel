40f5851446b898b2852391e75814f5f20fb45c39
Evening <evening@admins-iMac-2.local>
2023-01-18 18:13:15 +0800

    first commit

---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import SignIn from"./SignIn";
+import SignUp from"./SignUp";
+const AuthPage = () => {
+    const [isSignIn,setIsSignIn]=useState(true);
+  return (
+    <>{isSignIn ? (<SignIn setIsSignIn={setIsSignIn}/>):(
+
+        <SignUp setIsSignIn={setIsSignIn}/>
+    )}
+      </>
+  )
+}
+
+export default AuthPage;
