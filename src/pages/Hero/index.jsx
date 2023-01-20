d91c04d5b458763f110656b75a6640b85666c2b8
MuugiiU <classroom2leap@gmail.com>
2023-01-20 13:56:10 +0800

    hi

--- src/pages/Hero/index.jsx
+++ src/pages/Hero/index.jsx
@@ -1,17 +1,31 @@
-import { Container,Grid } from '@mui/material';
-import React from 'react'
-import DrawerAppBar from '../../components/navbar';
+import { Container, Grid } from "@mui/material";
+import React from "react";
+import DrawerAppBar from "../../components/navbar";
 
 const Hero = () => {
   return (
-    <Grid sx={{ width:'100%', position:"relative"}} >
-       <img src="/img/bgpic.png" width="100%" alt="" />
-        <DrawerAppBar/>
-       <Grid xs={12} lg={6} sx={{position:'absolute',top:"125px",color:'white',fontSize:'72px',left:'120px'}} >
-        <h1 style={{width: "1200px "}}>The whole world <span>awaits.</span></h1>
-       </Grid>
+    <Grid sx={{ width: "100%", position: "relative" }}>
+      <img src="/img/bgpic.png" width="100%" alt="" />
+      <DrawerAppBar />
+      <Container maxWidth="sm">
+        <Grid
+          xs={12}
+          lg={6}
+          sx={{
+            position: "absolute",
+            top: "125px",
+            color: "white",
+            fontSize: "72px",
+            left: "120px",
+          }}
+        >
+          <h1 style={{ width: "1200px " }}>
+            The whole world <span>awaits.</span>
+          </h1>
+        </Grid>
+      </Container>
     </Grid>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
