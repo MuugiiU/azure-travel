9a55896bd5c4f260aadf33eba4f823be2d0f1026
Evening <evening@admins-iMac-2.local>
2023-01-24 18:50:51 +0800

    new

--- src/components/Vacation/index.jsx
+++ src/components/Vacation/index.jsx
@@ -1,32 +1,42 @@
 import { Typography, Grid, Box } from "@mui/material";
 import React from "react";
+
+import { SwipeableDrawer } from "@mui/material";
 import vacationData from "../../data/vacation";
 const Vacation = () => {
   return (
-    <Grid sx={{ Top: "150px" }}>
+    <Grid sx={{ marginTop: "100px", position: "relative" }}>
       <Typography variant="h3" sx={{ color: "white", fontSize: "32px" }}>
         {" "}
         Top Vacation
       </Typography>
-      <Grid sx={{ display: "flex", gap: "40px" }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          gap: 5,
+          top: "70px",
+          position: "absolute",
+        }}
+      >
         {vacationData.map((vacationData, index) => {
           return (
             <Box
               sx={{
-                Top: "40px",
-                width: "200px",
+                top: "170px",
+                width: "300px",
                 height: "200px",
               }}
               key={index}
             >
-              <img src={vacationData.img} alt="" />
+              <img width="100%" height="100%" src={vacationData.img} alt="" />
               <Typography
                 variant="h3"
                 sx={{
                   color: "white",
-                  fontSize: "16px",
-                  position: "absolute",
-                  top: "600px",
+                  fontSize: "24px",
+                  position: " absolute",
+                  top: "160px",
                 }}
               >
                 {vacationData.title}
@@ -34,7 +44,7 @@ const Vacation = () => {
             </Box>
           );
         })}
-      </Grid>
+      </Box>
     </Grid>
   );
 };
