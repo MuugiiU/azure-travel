d31f8b52ca249bb05cf39208540731ae202bb119
Evening <evening@admins-iMac-2.local>
2023-01-30 18:35:07 +0800

    filter

--- src/data/Catalog/Beach.js
+++
@@ -1,6 +0,0 @@
-const BeachData=[{imgUTL:"../img/dalai.jpeg", title:"Beach House", bairsgil:"Vietnam", tovch:"Vieetnamese",id:"1"},
-{imgUTL:"../img/beach2.png", title:"Beach Bar", bairsgil:"Sanya China", tovch:"Bakeries",id:"2"},
-{imgUTL:"../img/beach3.png", title:"Beachhouse at the Moana", bairsgil:"United States", tovch:"American",id:"3"},
-{imgUTL:"../img/beach4.png", title:"Beach Club", bairsgil:"Kuala Mayaysia", tovch:"Bars",id:"4"},
-];
-export default BeachData;
--- src/pages/Categories/Beaches/index.jsx
+++ src/pages/Categories/Beaches/index.jsx
@@ -5,29 +5,31 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import { Button, Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import BeachData from "../../../data/Catalog/Beach";
+// import BeachData from "../../../data/Catalog/Beach";
 import { Link } from "react-router-dom";
 
-const Beach = (filteredData) => {
+const Beach = ({ filteredData }) => {
+  console.log(filteredData);
   return (
     <Grid sx={{ display: "flex", gap: "1rem", margin: "10%" }}>
       {filteredData.map((filterData, index) => (
         <Card key={index} sx={{ maxWidth: 345 }}>
           <CardMedia
             sx={{ height: 140, width: 400 }}
-            image={filteredData.imgUTL}
+            image={filterData.imgUTL}
             title="green iguana"
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              {filteredData.title}
+              {filterData.title}
             </Typography>
+
             <Typography variant="body2" color="text.secondary">
-              {filteredData.bairsgil}
+              {filterData.bairsgil}
             </Typography>
           </CardContent>
           <CardActions>
-            <Link href={"/Detials"}>{filteredData.tovch}</Link>
+            <Link href={"/Detials"}>{filterData.tovch}</Link>
           </CardActions>
         </Card>
       ))}
