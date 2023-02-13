e1973fba9c3e3ac3754aa737c2457482f5e3a8fa
MuugiiU <classroom2leap@gmail.com>
2023-02-13 21:36:17 +0800

    g

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -4,16 +4,16 @@ import Offser from "../../components/Main/Offser";
 import Browse from "../../components/Main/Browse";
 import Plan from "../../components/Main/Plan";
 import Connect from "../../components/Main/Connect";
-import Stepper from "../../components/stepper";
+import Steppers from "../../components/Stepper";
 
 const Main = () => {
   return (
     <Grid sm={12} md={6}>
       <Container>
         <Offser />
+        <Steppers />
         <Browse />
         <Plan />
-        <Stepper />
       </Container>
       <Connect />
     </Grid>
